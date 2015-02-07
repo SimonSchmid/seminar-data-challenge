32a50d398f6cbed14bce836fdecb5be085dde454
David Kolb <David.Kolb@uni-konstanz.de>
2015-02-07 01:32:37 +0100

    change HistogramCreator to work on table containing more than one distinct image

--- src/org/bof/HistogrammCreatorNodeModel.java
+++ src/org/bof/HistogrammCreatorNodeModel.java
@@ -2,6 +2,7 @@ package org.bof;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnDomainCreator;
@@ -26,6 +27,7 @@ import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.knip.base.data.img.ImgPlusCell;
 
 
 /**
@@ -50,8 +52,6 @@ public class HistogrammCreatorNodeModel extends NodeModel {
 
     /** initial default count value. */
     static final int DEFAULT_COUNT = 1;
-
-
 	public static final int IN_PORT = 0;
 
     // example value: the models count variable filled from the dialog 
@@ -90,18 +90,10 @@ public class HistogrammCreatorNodeModel extends NodeModel {
             final ExecutionContext exec) throws Exception {
 
         // TODO do something here
-        logger.info("Node Model Stub... this is not yet implemented !");
-
+        logger.info("Node Model Stub... this is not yet implemented !");       
         
-        // the data table spec of the single output table, 
-        // the table will have three columns:
-        DataColumnSpec[] allColSpecs = new DataColumnSpec[2];
-        allColSpecs[0] = 
-            new DataColumnSpecCreator("Row", StringCell.TYPE).createSpec();
-        allColSpecs[1] = 
-            new DataColumnSpecCreator("HistoCount", IntCell.TYPE).createSpec();
+        DataTableSpec outputSpec = createOutputColumnSpec();
         
-        DataTableSpec outputSpec = new DataTableSpec(allColSpecs);
         // the execution context will provide us with storage capacity, in this
         // case a data container to which we will add rows sequentially
         // Note, this container can also handle arbitrary big data tables, it
@@ -110,43 +102,86 @@ public class HistogrammCreatorNodeModel extends NodeModel {
         
         int[] histoCount = new int[m_range.getIntValue()];      
         
-        int colIndex = inData[IN_PORT].getDataTableSpec().findColumnIndex(
+        int colIndexCluster = inData[IN_PORT].getDataTableSpec().findColumnIndex(
                 m_column.getStringValue());
         
-        for (DataRow currRow : inData[IN_PORT]) {
-        	StringCell currCell = (StringCell)currRow.getCell(colIndex);
-        	String currValue = currCell.getStringValue();
-        	
-        	String prePattern = currValue.substring(0, m_pattern.getStringValue().length());
-        	String postPattern = currValue.substring(m_pattern.getStringValue().length());
-        	    	
-        	if (prePattern.equals(m_pattern.getStringValue()) == false) {
-                throw new InvalidSettingsException(
-                        "wrong pattern specified: " 
-                        + m_pattern.getStringValue() 
-                        + " . Please (re-)configure the node.");
-            }
-        	else{
-        		Integer currHistoCount = new Integer(postPattern);
-        		histoCount[currHistoCount]++;
-        	}
-        }
+        int colIndexImage = inData[IN_PORT].getDataTableSpec().findColumnIndex(
+                "Image");
         
-        for (int i = 0; i < m_range.getIntValue(); i++) {
-            RowKey key = new RowKey("Row " + i);
-            // the cells of the current row, the types of the cells must match
-            // the column spec (see above)
-            DataCell[] cells = new DataCell[2];
-            cells[0] = new StringCell("cluster_" + i); 
-            cells[1] = new IntCell(histoCount[i]);     
+        ArrayList<DataRow> distinctImg = new ArrayList<DataRow>();
+        ArrayList< ArrayList<DataRow> > distinctImgs = new ArrayList< ArrayList<DataRow> >();    
+        long oldHash = 0;
+        
+        int rowCount = 1;
+        for (DataRow currRow : inData[IN_PORT]) {
+        	ImgPlusCell<?> img = (ImgPlusCell<?>)currRow.getCell(colIndexImage);
+        	long newHash = img.hashCode();
+        	
+        	//end of table reached
+        	if( rowCount == inData[IN_PORT].getRowCount() ){
+        		distinctImg.add(currRow);
+        		distinctImgs.add(new ArrayList<DataRow>(distinctImg));
+        	}
+        	//curr img equal to img before
+        	else if( oldHash == 0 || newHash == oldHash  ){
+        		distinctImg.add(currRow);
+        	}        	
+        	//curr img not equal to img before
+        	else{
+        		distinctImgs.add(new ArrayList<DataRow>(distinctImg));
+        		distinctImg.clear();
+        		distinctImg.add(currRow);
+        	}  	
+        	oldHash = newHash;
+        	rowCount++;
+        }
+       
+        
+        int rowCurser = 0;
+        for(ArrayList<DataRow> img : distinctImgs){
+        	for (DataRow currRow : img) {
+            	StringCell currCell = (StringCell)currRow.getCell(colIndexCluster);
+            	String currValue = currCell.getStringValue();
+            	
+            	String prePattern = currValue.substring(0, m_pattern.getStringValue().length());
+            	String postPattern = currValue.substring(m_pattern.getStringValue().length());
+            	    	
+            	if (prePattern.equals(m_pattern.getStringValue()) == false) {
+                    throw new InvalidSettingsException(
+                            "wrong pattern specified: " 
+                            + m_pattern.getStringValue() 
+                            + " . Please (re-)configure the node.");
+                }
+            	else{
+            		Integer currHistoCount = new Integer(postPattern);
+            		histoCount[currHistoCount]++;
+            	}
+            }
+            
+            
+            RowKey key = new RowKey("Row " + rowCurser); 
+                
+            DataCell[] cells = new DataCell[m_range.getIntValue()];
+            for(int k = 0 ; k < m_range.getIntValue() ; k++){
+                cells[k] = new IntCell(histoCount[k]);
+            }
             DataRow row = new DefaultRow(key, cells);
             container.addRowToTable(row);
-            
+                
+            //clear histoCount Array for next iteration
+            for(int j = 0 ; j < m_range.getIntValue() ; j++){
+                histoCount[j] = 0;
+            }
+                
             // check if the execution monitor was canceled
             exec.checkCanceled();
-            exec.setProgress(i / (double)m_range.getIntValue(), 
-                "Adding row " + i);
+            exec.setProgress(rowCurser / (double)m_range.getIntValue(), 
+                "Adding row " + rowCurser);
+            
+            rowCurser++;
         }
+        
+        
         // once we are done, we close the container and return its table
         container.close();
         BufferedDataTable out = container.getTable();
@@ -196,39 +231,19 @@ public class HistogrammCreatorNodeModel extends NodeModel {
     	
         // and the DataTableSpec for the appended part
         DataTableSpec appendedSpec = createOutputColumnSpec();
-        // since it is only appended the new output spec contains both:
-        // the original spec and the appended one
-        DataTableSpec outputSpec = new DataTableSpec(inSpecs[IN_PORT],
-                appendedSpec);
-             
+                     
         return new DataTableSpec[]{appendedSpec};
     }
 
     
     private DataTableSpec createOutputColumnSpec() {
-        // we want to add a column with the number of the bin 
-        DataColumnSpecCreator colSpecCreatorString = new DataColumnSpecCreator(
-                "Row", StringCell.TYPE);
+    	DataColumnSpec[] allColSpecs = new DataColumnSpec[m_range.getIntValue()];
+        for(int i = 0 ; i < m_range.getIntValue() ; i++ ){
+        	allColSpecs[i] = new DataColumnSpecCreator("cluster_" + i, IntCell.TYPE).createSpec();
+        }       
+        DataTableSpec outputSpec = new DataTableSpec(allColSpecs);
         
-        DataColumnSpecCreator colSpecCreatorInt = new DataColumnSpecCreator(
-                "HistoCount", IntCell.TYPE);
-        
-        // if we know the number of bins we also know the number of possible
-        // values of that new column
-        DataColumnDomainCreator domainCreator = new DataColumnDomainCreator(
-                new IntCell(0), new IntCell(m_range.getIntValue() - 1));
-        // and can add this domain information to the output spec
-        colSpecCreatorInt.setDomain(domainCreator.createDomain());
-        colSpecCreatorString.setDomain(domainCreator.createDomain());
-        
-        // now the column spec can be created
-        DataColumnSpec newColumnSpecInt = colSpecCreatorInt.createSpec();
-        DataColumnSpec newColumnSpecString = colSpecCreatorString.createSpec();
-        
-        DataTableSpec newColumnSpec = new DataTableSpec(newColumnSpecString,
-        		newColumnSpecInt);
-        
-        return newColumnSpec;
+        return outputSpec;
     }
     
     
